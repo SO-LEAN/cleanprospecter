4169608cfc839ef060aacca28ee2da3e5b30501c
Michel MAIER <michel.maier@gmail.com>
2018-03-18 10:39:30 +0100

    remove facade

--- src/CleanProspecter/Gateway/Database/DataBaseGateway.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Solean\CleanProspector\Gateway\Database;
-
-interface DatabaseGateway extends TransactionGateway, UserGateway, ProspectGateway
-{
-
-}
--- src/CleanProspecter/Gateway/Database/TransactionGateway.php
+++ src/CleanProspecter/Gateway/Database/TransactionGateway.php
@@ -3,7 +3,5 @@ namespace Solean\CleanProspector\Gateway\Database;
 
 interface TransactionGateway
 {
-    public function startTransaction() : void;
     public function commit() : void;
-    public function rollback() : void;
 }
